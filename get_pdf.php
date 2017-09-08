4a21eeb8447d8d87d727e377b158b1616a843f5a
k8thegreat <emikryukova@gmail.com>
2017-09-08 15:27:50 +0300

    dompdf

---
+++ get_pdf.php
@@ -0,0 +1,20 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+set_include_path(get_include_path() . PATH_SEPARATOR . "/local/php_interface/include/dompdf");
+
+require_once "dompdf_config.inc.php";
+
+$dompdf = new DOMPDF();
+
+$html = <<<'ENDHTML'
+<html>
+ <body>
+  <h1>Hello Dompdf</h1>
+ </body>
+</html>
+ENDHTML;
+
+$dompdf->load_html($html);
+$dompdf->render();
+
+$dompdf->stream("hello.pdf");
